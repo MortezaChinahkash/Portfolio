93a9b5990f6eb79b72f26d35d3acb0934a60e121
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-05-31 12:35:25 +0200

    refactor: implement skills section with dynamic skill rendering

--- src/app/main-component/skills/skills.component.ts
+++ src/app/main-component/skills/skills.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  name: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-skills',
   imports: [],
@@ -8,4 +13,54 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  skills: Skill[] = [
+    { 
+      name: 'HTML', 
+      image: 'assets/images/skills/html.png' 
+    },
+    { 
+      name: 'CSS', 
+      image: 'assets/images/skills/css.png' 
+    },
+    { 
+      name: 'JavaScript', 
+      image: 'assets/images/skills/javascript.png' 
+    },
+    { 
+      name: 'Angular', 
+      image: 'assets/images/skills/angular.png' 
+    },
+    { 
+      name: 'React', 
+      image: 'assets/images/skills/react.png' 
+    },
+    {
+      name: 'Bootstrap',
+      image: 'assets/images/skills/bootstrap.png'
+    },
+    {
+      name: 'Material Design',
+      image: 'assets/images/skills/material.png'
+    },
+    {
+      name: 'Git',
+      image: 'assets/images/skills/git.png'
+    },
+    {
+      name: 'GitHub',
+      image: 'assets/images/skills/github.png'
+    },
+    {
+      name: 'SASS',
+      image: 'assets/images/skills/sass.png'
+    },
+    {
+      name: 'TypeScript',
+      image: 'assets/images/skills/typescript.png'
+    },
+    {
+      name: 'SCSS',
+      image: 'assets/images/skills/scss.png'
+    }
+  ];
 }
