784058f9dff5c296bbc817ad4c97fb68983853e0
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-02 00:35:20 +0200

    feat: add project data structure and initialize projects array in portfolio component

--- src/app/main-component/portfolio/portfolio.component.ts
+++ src/app/main-component/portfolio/portfolio.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
 
+interface PortfolioItem {
+  title: string;
+  description: string;
+  imageUrl: string;
+  projectUrl: string;
+  gitHubUrl: string;
+  technologies: { name: string; imageUrl: string }[];
+  isFeatured: boolean;
+  isInProgress: boolean;
+}
+
 @Component({
   selector: 'app-portfolio',
   imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-
+  projects: PortfolioItem[] = [
+    {
+      title: 'Join',
+      description: 'Description coming soon',
+      imageUrl: 'null',
+      projectUrl: 'null',
+      gitHubUrl: 'null',
+      technologies: [
+        {
+          name: 'Angular',
+          imageUrl: '',
+        },
+        {
+          name: 'TypeScript',
+          imageUrl: '',
+        },
+        {
+          name: 'SCSS',
+          imageUrl: '',
+        },
+      ],
+      isFeatured: true,
+      isInProgress: true,
+    },
+    {
+      title: 'El Pollo Loco',
+      description: 'Description for project 2',
+      imageUrl: 'https://via.placeholder.com/150',
+      projectUrl: 'https://example.com/project2',
+      gitHubUrl: 'https://github.com/user/project2',
+      technologies: ['React', 'JavaScript'],
+      isFeatured: false,
+      isInProgress: true,
+    },
+  ];
 }
