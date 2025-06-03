7c89106198dcd1d18000caf624569df18d9f2b52
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-03 10:22:06 +0200

    feat: add comments component to main component

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -4,10 +4,11 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @Component({
   selector: 'app-main-component',
-  imports: [ ATFComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [ ATFComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent, CommentsComponent],
   templateUrl: './main-component.component.html',
   styleUrl: './main-component.component.scss'
 })
