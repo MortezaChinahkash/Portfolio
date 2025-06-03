7b281c496e724576e6c413e92f001f3a43f6047b
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-03 10:28:12 +0200

    feat: define Comment interface and initialize comments array in CommentsComponent

--- src/app/main-component/comments/comments.component.ts
+++ src/app/main-component/comments/comments.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Comment {
+  name: string;
+  text: string;
+  date: string;
+  linkedIn: string;
+}
+
 @Component({
   selector: 'app-comments',
   imports: [],
@@ -7,5 +14,26 @@ import { Component } from '@angular/core';
   styleUrl: './comments.component.scss'
 })
 export class CommentsComponent {
+
+  comments: Comment[] = [
+    {
+      name: 'John Doe',
+      text: 'Great portfolio! I really liked the project on XYZ.',
+      date: '2023-01-01',
+      linkedIn: 'https://www.linkedin.com/in/johndoe',
+    },
+    {
+      name: 'Jane Smith',
+      text: 'Impressive work! The design of ABC is stunning.',
+      date: '2023-01-02',
+      linkedIn: 'https://www.linkedin.com/in/janesmith',
+    },
+    {
+      name: 'Alice Johnson',
+      text: 'Your skills in Angular are top-notch! Keep it up!',
+      date: '2023-01-03',
+      linkedIn: 'https://www.linkedin.com/in/alicejohnson',
+    }
+  ];
 
 }
