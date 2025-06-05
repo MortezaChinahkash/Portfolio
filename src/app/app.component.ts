a1e6e54e4519a3506e47d6cc34de1bf1f576b0ae
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-06 00:32:10 +0200

    fix: update footer component integration and adjust header styles with mix-blend-mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { TranslationService } from './shared/services/translation.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-component/contact/contact.component.ts
+++ src/app/main-component/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslationService } from '../../shared/services/translation.service';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
