cee1fab6180832e07eba53b68af2cde8255369f3
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-11 03:41:50 +0200

    feat: add imprint route and update footer link to use routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponentComponent } from './main-component/main-component.componen
 import { ProjectDabubbleComponent } from './projects/project-dabubble/project-dabubble.component';
 import { ProjectJoinComponent } from './projects/project-join/project-join.component';
 import { ProjectElPolloLocoComponent } from './projects/project-el-pollo-loco/project-el-pollo-loco.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
     {path: '', component: MainComponentComponent},
     {path: 'projects/project-dabubble', component: ProjectDabubbleComponent},
     {path: 'projects/project-join', component: ProjectJoinComponent},
-    {path: 'projects/project-el-pollo-loco', component: ProjectElPolloLocoComponent}
+    {path: 'projects/project-el-pollo-loco', component: ProjectElPolloLocoComponent},
+    {path: 'imprint', component: ImprintComponent}
 ];
