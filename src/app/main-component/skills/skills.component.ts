d6cc53010f68e11eb35b7fe87fd548ee874268d4
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-24 01:19:28 +0200

    feat: add learning skills section with responsive design and update note styles

--- src/app/main-component/skills/skills.component.ts
+++ src/app/main-component/skills/skills.component.ts
@@ -18,23 +18,25 @@ export class SkillsComponent {
   get ringClass(): string {
     return this.translationService.currentLang === 'de' ? 'german' : 'english';
   }
-
   skills: Skill[] = [
     { name:'HTML',       image:'assets/png/icons/Skill Icons/HTML.png' },
     { name:'CSS',        image:'assets/png/icons/Skill Icons/CSS.png' },
     { name:'JavaScript', image:'assets/png/icons/Skill Icons/JavaScript.png'  },
     { name:'Angular',    image:'assets/png/icons/Skill Icons/Angular.png' },
-    { name:'React',      image:'assets/png/icons/Skill Icons/React.png' },
     { name:'Bootstrap',  image:'assets/png/icons/Skill Icons/Bootstrap.png' },
     { name:'Material',   image:'assets/png/icons/Skill Icons/Material Design.png' },
     { name:'Git',        image:'assets/png/icons/Skill Icons/Git.png' },
     { name:'GitHub',     image:'assets/png/icons/Skill Icons/GitHub.png' },
     { name:'SASS',       image:'assets/png/icons/Skill Icons/SASS.png' },
     { name:'TypeScript', image:'assets/png/icons/Skill Icons/TypeScript.png' },
-    { name:'Vue.js',     image:'assets/png/icons/Skill Icons/Vue.Js.png' },
     { name:'Firebase',   image:'assets/png/icons/Skill Icons/Firebase.png' },
     { name:'REST API',   image:'assets/png/icons/Skill Icons/Rest-Api.png' },
     { name:'Scrum',      image:'assets/png/icons/Skill Icons/Scrum.png' }
+  ];
+
+  learningSkills: Skill[] = [
+    { name:'React',      image:'assets/png/icons/Skill Icons/React.png' },
+    { name:'Vue.js',     image:'assets/png/icons/Skill Icons/Vue.Js.png' }
   ];
 
   private boundMove = (e: MouseEvent) => this.onMouseMove(e);
